949fccb88b222590a13f0bd506c08d2e96a0d471
Santosh Srinivas <santosh@mypad.in>
2021-10-09 10:15:51 +0530

    site config for cheetah realtors reimagined

--- site.config.js
+++ site.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '067dd719a912471ea9a3ac10710e7fdf',
+  rootNotionPageId: 'https://mypad.notion.site/Cheetah-Realtors-Reimagined-d542d597977c43709a9f8448861cbf0a',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
